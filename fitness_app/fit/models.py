2a913d361126616d8fc83d498f4c339de5e0a63e
sumedhasmile <sumedhasmileok@gmail.com>
2020-10-18 13:34:44 +0530

    commit

--- fitness_app/fit/models.py
+++ fitness_app/fit/models.py
@@ -1,8 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
 # Create your models here.
+
+
+options = [
+    ['breakfast', 'breakfast'],
+    ['lunch', 'lunch'],
+    ['dinner', 'dinner'],
+    ['Miscellaneous', 'Miscellaneous'],
+]
+
 
 class User_details(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
@@ -29,9 +37,10 @@ class Goals(models.Model):
 
 class Food_details(models.Model):
     user = models.ForeignKey(User_details, on_delete=models.CASCADE, null=True)
-    calories = models.CharField(max_length=100, null=True)
+    category = models.CharField(max_length=500, choices=options)
     food_type = models.CharField(max_length=500, null=True)
-    calculate = models.IntegerField(null=True)
+    calorie = models.DecimalField(max_digits=5, decimal_places=2, default=0, blank=True)
+    quantity = models.IntegerField(default=1, null=True, blank=True)
 
     def __str__(self):
         return self.food_type
--- fitness_app/fit/views.py
+++ fitness_app/fit/views.py
@@ -1,24 +1,27 @@
 from django.contrib import messages
-from django.contrib.auth import authenticate, login , logout
+from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
 from .models import *
 from django.contrib.auth.models import User
 
 
-
 def index(request):
-    return render(request , 'fit/home.html', {'text':"Hello "})
+    return render(request, 'fit/home.html', {'text': "Hello "})
+
 
 def login_get(request):
-    return render(request,'fit/login.html')
+    return render(request, 'fit/login.html')
+
 
 def signup_get(request):
-    return render(request,'fit/signup.html')
+    return render(request, 'fit/signup.html')
+
 
 def logout_get(request):
     logout(request)
-    print(User.first_name,"logged out")
-    return render(request,'fit/home.html')
+    print(User.first_name, "logged out")
+    return render(request, 'fit/home.html')
+
 
 def Signup(request):
     if request.method == "POST":
@@ -37,13 +40,14 @@ def Signup(request):
         userdata = User.objects.filter(username=u)
         if userdata:
             messages.error(request, 'User Already Exists')
-            return render(request,'fit/login.html')
+            return render(request, 'fit/login.html')
         else:
             user = User.objects.create_user(first_name=f, last_name=l, username=u, password=p, email=email)
             User_details.objects.create(user=user, mobile_no=mob, profile_image=img, age=age, gender=gender,
                                         weight=weight, heigth=height)
-            return render(request,'fit/login.html')
+            return render(request, 'fit/login.html')
     return render(request, 'fit/signup.html')
+
 
 def Login(request):
     if request.method == "POST":
@@ -53,13 +57,25 @@ def Login(request):
         user = authenticate(username=u, password=p)
         if user:
             login(request, user)
-            #return redirect('home')
-            return render(request,'fit/main_page.html')
+            return render(request, 'fit/main_page.html')
         else:
             messages.error(request, 'User not found')
             context = {
                 "message": "User not found"
             }
-            return render(request,'fit/login.html',context)
+            return render(request, 'fit/login.html', context)
 
     return render(request, 'fit/login.html')
+
+
+def Add_task(request, id):
+    task = Goals.objects.filter(user=request.user)
+    if request.method == "POST":
+
+        dic = request.POST
+        tasks = dic["title"]
+        date = dic["task_date"]
+        due_date = dic["due_date"]
+        Goals.objects.create(user=request.user, task_name=tasks, Due_date=due_date, Task_date=date)
+    d = {"task": task}
+    return render(request, 'fit/add_task.html', d)
